1a1d75a58fb7a086945a6c7065305a0ff924fcb0
Amit Chowdhury <amit@nestedapps.com>
2016-11-21 14:57:07 +0600

    Initial Commit

---
+++ DynamicCollectionViewCell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DynamicCollectionViewCell
+//
+//  Created by Amit Chowdhury on 11/21/16.
+//  Copyright © 2016 Amit Chowdhury. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DynamicCollectionViewCell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DynamicCollectionViewCell
+//
+//  Created by Amit Chowdhury on 11/21/16.
+//  Copyright © 2016 Amit Chowdhury. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
